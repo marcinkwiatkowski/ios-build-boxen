bc3f88d274cb06d8a2fb782249df7aebaacb8055
Marcin Kwiatkowski <marcin.kwiatkowski@hotmail.com>
2013-05-12 10:54:19 +0100

    comment out more crap

--- manifests/site.pp
+++ manifests/site.pp
@@ -50,9 +50,7 @@ Homebrew::Formula <| |> -> Package <| |>
 
 node default {
   # core modules, needed for most things
-  include dnsmasq
   include git
-  include hub
   include nginx
 
   # fail if FDE is not enabled
